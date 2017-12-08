0f93d09bf6036bf7577c5e32ad9df43c8bd47ce9
CulicovIgor <igory95@mail.ru>
2017-12-08 12:44:40 +0200

    half working

--- PAD_Lab3-4/src/controller/proxy_controller.py
+++ PAD_Lab3-4/src/controller/proxy_controller.py
@@ -14,25 +14,72 @@ class Proxy(object):
             self.list_of_addresses.append(address['address'])
         print(self.list_of_addresses)
 
+    def handle_get(self):
+        requested_url = urlp.urlparse(cherrypy.url())
+        print("url")
+        print(requested_url)
+        url = "http://" + \
+              self.list_of_addresses[
+                  random.randint(
+                      0, len(self.list_of_addresses) - 1)] + requested_url.path
+        print(url)
+        resp = requests.get(url, cherrypy.request.params, headers=cherrypy.request.headers)
+        cherrypy.response.headers['Content-Type'] = resp.headers['Content-Type']
+        return resp
+
+    def handle_put(self):
+        requested_url = urlp.urlparse(cherrypy.url())
+        print("url")
+        print(requested_url)
+        url = "http://" + \
+              self.list_of_addresses[
+                  random.randint(
+                      0, len(self.list_of_addresses) - 1)] + requested_url.path
+        resp = requests.get(url, cherrypy.request.params, headers=cherrypy.request.headers)
+        return resp
+
+    def handle_patch(self):
+        requested_url = urlp.urlparse(cherrypy.url())
+        print("url")
+        print(requested_url)
+        url = "http://" + \
+              self.list_of_addresses[
+                  random.randint(
+                      0, len(self.list_of_addresses) - 1)] + ""
+        resp = requests.get(url, cherrypy.request.params)
+        return resp
+
+    def handle_post(self):
+        requested_url = urlp.urlparse(cherrypy.url())
+        print("url")
+        print(requested_url)
+        url = "http://" + \
+              self.list_of_addresses[
+                  random.randint(
+                      0, len(self.list_of_addresses) - 1)] + requested_url
+        resp = requests.post(url, cherrypy.request.body.read().decode(), headers=cherrypy.request.headers)
+        return resp
+
     @cherrypy.expose
     def default(self, *args, **kwargs):
         print("params")
         print(cherrypy.request.params)
         print("headers")
         print(cherrypy.request.headers)
+        if cherrypy.request.method == "PATCH":
+            return self.handle_patch()
+        if cherrypy.request.method == "PUT":
+            return self.handle_put()
+        if cherrypy.request.method == "GET":
+            return self.handle_get()
         if cherrypy.request.method == "POST":
-            requested_url = urlp.urlparse(cherrypy.url())
-            print(requested_url)
-            url = "http://" + \
-                  self.list_of_addresses[
-                      random.randint(
-                          0, len(self.list_of_addresses) - 1)] + "/api"
-            resp = requests.post(url, cherrypy.request.body.read().decode())
-            return resp
-        return "Hello nigga!"
+            return self.handle_post()
+        return "Hello body!"
 
     @cherrypy.expose
     def index(self, *args, **kwargs):
+        print("headers")
+        print(cherrypy.request.headers)
         return "It doesn't work!"
 
     @cherrypy.expose
